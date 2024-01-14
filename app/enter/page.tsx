450664136b7e25fa6827e3dd835e1db0f70c98ca
TaeHyeon <smile_sud@naver.com>
2024-01-14 17:42:58 +0900

    Upload Item page(2)

--- app/enter/page.tsx
+++ app/enter/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import React from 'react';
+import React from "react";
 
 function cls(...classnames: string[]) {
   return classnames.join(" ");
@@ -46,13 +46,14 @@ export default function Enter() {
         </div>
 
         <form className="flex flex-col mt-8">
-          <label className="text-sm font-medium text-gray-700">
+          <label htmlFor="input" className="text-sm font-medium text-gray-700">
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
           <div className="mt-1">
             {method === "email" ? (
               <input
+                id="input"
                 type="email"
                 required
                 className="appearance-none w-full py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
@@ -64,6 +65,7 @@ export default function Enter() {
                   +82
                 </span>
                 <input
+                  id="input"
                   type="number"
                   className="appearance-none w-full py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                   required
