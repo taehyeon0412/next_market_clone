5e9066c6a87a57d2e61eeb07540ac41a7c31c017
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2024-01-18 22:19:43 +0900

    Community(1)

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
             <div className="w-20 h-20 bg-gray-400 rounded-md" />
             <div className="pt-2 flex flex-col">
               <h3 className="text-sm font-medium text-gray-900">
-                New iPhone 14
+                New IPhone 14
               </h3>
               <span className="text-xs text-gray-500">Black</span>
               <span className="font-medium mt-1 text-gray-900">$95</span>
