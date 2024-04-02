7105649abba8e58def063bcecb5c92146ae870f3
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2024-04-02 21:30:19 +0900

    미들웨어(2)
    1.로그인 하지 않은 사용자가 강제로 퍼블릭 페이지 이외의 페이지에 들어가려고 할 때 미들웨어가 강제로 redirect함
    
    2.로그인 한 사용자가 강제로 퍼블릭 페이지에 들어가려고 할때 미들웨어가 강제로 redirect함

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
+import getSession from "./app/_libs/_server/session";
 
-export function middleware(request: NextRequest) {
-  console.log("hello");
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/enter": true,
+  "/sms": true,
+  "/create-account": true,
+};
+//퍼블릭으로 접근 가능한 URL 목록
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname]; //유저가 publicOnlyUrls로 이동하는지 알 수 있게함
+
+  //로그인하지 않은 상태
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+  //로그인 한 상태
+  else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/home", request.url));
+    }
+  }
 }
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+//미들웨어가 실행되지 않는것
