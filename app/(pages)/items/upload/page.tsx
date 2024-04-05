0c92087ce37fe545f18a00158e86d0b281049d94
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2024-04-05 23:33:07 +0900

    아이템 업로드 (2)
    -파일 최대 사이즈 4mb로 제한
    -이미지만 업로드 가능하게 함
    -이미지 미리보기 없애기 버튼 추가

--- app/(pages)/items/upload/page.tsx
+++ app/(pages)/items/upload/page.tsx
@@ -4,10 +4,23 @@ import Button from "@/app/_components/button";
 import Input from "@/app/_components/input";
 import Layout from "@/app/_components/layout";
 import TextArea from "@/app/_components/textarea";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { uploadItem } from "./action";
+import { MB } from "@/app/_libs/_client/utils";
 
 export default function Upload() {
   const [preview, setPreview] = useState("");
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const isOversizeImage = (file: File): boolean => {
+    if (file.size > 4 * MB) {
+      alert("파일 크기가 4MB를 초과했습니다.");
+      return true;
+    }
+    return false;
+  };
+  //파일 크기 검사
+
   const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { files },
@@ -20,15 +33,33 @@ export default function Upload() {
     const file = files[0];
     // 파일 목록에서 첫 번째 파일을 가져옴
 
+    if (!file.type.startsWith("image/")) {
+      alert("이미지 파일만 업로드 가능합니다.");
+      return;
+    }
+    // 파일 타입 검사
+
+    if (isOversizeImage(file)) {
+      return;
+    }
+
     const url = URL.createObjectURL(file);
     //파일을 임시 URL로 만들어줌
     setPreview(url);
   };
   //사진 추가 로직
 
+  const removeImage = () => {
+    setPreview("");
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+  };
+  //이미지 미리보기 삭제 로직
+
   return (
     <Layout canGoBack>
-      <div className="relative px-4 pt-4 mb-5 h-full">
+      <form action={uploadItem} className="relative px-4 pt-4 mb-5 h-full">
         <div>
           <label
             className="w-full text-gray-600 hover:cursor-pointer hover:border-orange-500 hover:text-orange-500 flex flex-col items-center justify-center border-2 border-dashed border-gray-300 h-60 sm:h-96  rounded-md bg-contain bg-center bg-no-repeat"
@@ -54,15 +85,43 @@ export default function Upload() {
               </>
             ) : null}
 
-            <input onChange={onImageChange} className="hidden" type="file" />
+            <input
+              onChange={onImageChange}
+              name="photo"
+              className="hidden z-30"
+              type="file"
+              accept="image/*"
+            />
             {/* label로 input을 감싸고 hidden으로 input을 감춰주면 이쁜 input이 된다 */}
           </label>
+
+          {preview === "" ? null : (
+            <button
+              onClick={removeImage}
+              className="z-40 absolute top-4 right-4"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="white"
+                className="w-6 h-6 bg-black rounded-full"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            </button>
+          )}
         </div>
 
         <Input
           required
           label="제목"
-          name="name"
+          name="title"
           type="text"
           placeholder="제목"
         />
@@ -71,18 +130,22 @@ export default function Upload() {
           label="가격"
           name="price"
           placeholder="가격을 입력해주세요."
-          type="text"
+          type="number"
           kind="price"
         />
 
         <div className="mt-5 pb-20 block text-sm font-medium">
-          <TextArea name="description" label="자세한 설명" />
+          <TextArea
+            name="description"
+            label="자세한 설명"
+            labelName="textArea"
+          />
         </div>
 
         <div className="fixed py-2 px-4 bottom-0 mx-auto left-0 right-0 max-w-lg z-50 bg-white border border-white">
           <Button text="작성 완료" />
         </div>
-      </div>
+      </form>
     </Layout>
   );
 }
--- app/_libs/_client/utils.tsx
+++ app/_libs/_client/utils.tsx
@@ -1,3 +1,5 @@
 export function cls(...classnames: string[]) {
   return classnames.join(" ");
 }
+
+export const MB = 1024 * 1024;
