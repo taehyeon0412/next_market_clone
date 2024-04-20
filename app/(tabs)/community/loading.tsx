f618b87cee985fed7a2eb445d46bd504487adee1
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2024-04-21 02:46:00 +0900

    동내생활 페이지(2)
    -loading.tsx 추가
    -page.tsx
     -백엔드 통신 추가

---
+++ app/(tabs)/community/loading.tsx
@@ -0,0 +1,65 @@
+import Layout from "@/app/_components/layout-bar";
+
+export default function Loading() {
+  return (
+    <>
+      <Layout title="동네생활" hasTabBar />
+      <div className="p-5 animate-pulse flex flex-col gap-5 pb-20">
+        {[...Array(10)].map((_, i) => (
+          <div
+            key={i}
+            className="px-4 flex flex-col items-start cursor-pointer"
+          >
+            <div className="flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-neutral-700 h-5 w-20"></div>
+
+            <div className="mt-2 bg-neutral-700 h-5 w-96"></div>
+
+            <div className="mt-5 flex w-full items-center justify-between text-gray-500 font-medium text-xs">
+              <div className="bg-neutral-700 h-5 w-10"></div>
+              <div className="bg-neutral-700 h-5 w-10"></div>
+            </div>
+
+            <div className="flex space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px] w-full">
+              <span className="flex space-x-2 items-center text-sm">
+                <svg
+                  className="w-4 h-4"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  ></path>
+                </svg>
+
+                <div className="bg-neutral-700 h-5 w-10"></div>
+              </span>
+
+              <span className="flex space-x-2 items-center text-sm">
+                <svg
+                  className="w-4 h-4"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                  ></path>
+                </svg>
+                <div className="bg-neutral-700 h-5 w-10"></div>
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
--- app/(tabs)/community/page.tsx
+++ app/(tabs)/community/page.tsx
@@ -1,17 +1,48 @@
 import FloatingButton from "@/app/_components/floating-button";
 import Layout from "@/app/_components/layout-bar";
-import type { NextPage } from "next";
+import { formatToTimeAgo } from "@/app/_libs/_client/utils";
+import db from "@/app/_libs/_server/db";
 import Link from "next/link";
 
-const Community: NextPage = () => {
+async function getPosts() {
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+
+      user: {
+        select: {
+          username: true,
+        },
+      },
+      _count: {
+        select: {
+          likes: true,
+          comments: true,
+        }, //_count는 post를 가리키는 요소의 개수를 카운팅 해줌
+      },
+    },
+  });
+
+  return posts;
+}
+//db 백엔드 연결
+
+export default async function Community() {
+  const posts = await getPosts();
+  console.log(posts);
+
   return (
     <>
-      <Layout hasTabBar title="동내생활" />
-      <div className="space-y-8">
-        {[1, 2, 3, 4, 5, 6].map((_, i) => (
+      <Layout hasTabBar title="동네생활" />
+      <div className="space-y-8 pb-20">
+        {posts.map((post) => (
           <Link
-            href={`/community/${i}`}
-            key={i}
+            href={`/community/${post.id}`}
+            key={post.id}
             className="px-4 flex flex-col items-start cursor-pointer"
           >
             <span className="flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-200 text-gray-800">
@@ -19,52 +50,60 @@ const Community: NextPage = () => {
             </span>
 
             <div className="mt-2 text-gray-700">
-              <span className="text-orange-500 font-medium">Q.</span> What is
-              the best mandu restaurant?
+              <span className="text-orange-500 font-medium">Q.</span>
+              <span className="pl-2 font-medium">{post.title}</span>
             </div>
 
             <div className="mt-5 flex w-full items-center justify-between text-gray-500 font-medium text-xs">
-              <span>사용자</span>
-              <span>18시간 전</span>
+              <span>{post.user.username}</span>
+              <span>{formatToTimeAgo(post.created_at.toString())}</span>
             </div>
 
-            <div className="flex space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px] w-full">
-              <span className="flex space-x-2 items-center text-sm">
-                <svg
-                  className="w-4 h-4"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                  ></path>
-                </svg>
+            <div className="flex justify-between mt-3 text-gray-700 py-2.5 border-t border-b-[2px] w-full">
+              <div className="flex space-x-5">
+                <span className="flex space-x-2 items-center text-sm">
+                  <svg
+                    className="w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                  </svg>
 
-                <span>궁금해요 1</span>
-              </span>
+                  <span>궁금해요 {post._count.likes}</span>
+                </span>
+                {/* 좋아요 수 */}
 
-              <span className="flex space-x-2 items-center text-sm">
-                <svg
-                  className="w-4 h-4"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                  ></path>
-                </svg>
-                <span>답변 1</span>
-              </span>
+                <span className="flex space-x-2 items-center text-sm">
+                  <svg
+                    className="w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                    ></path>
+                  </svg>
+                  <span>답변 {post._count.comments}</span>
+                </span>
+                {/* 코멘트 수 */}
+              </div>
+
+              <div>
+                <span className="text-sm">조회수 {post.views}</span>
+              </div>
             </div>
           </Link>
         ))}
@@ -88,6 +127,4 @@ const Community: NextPage = () => {
       </div>
     </>
   );
-};
-
-export default Community;
+}
